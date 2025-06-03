66d41d7911c4596d29e9367bf5a43de2c3cad6a0
fwrkin <fedotovvva23@mail.ru>
2025-06-03 15:56:28 +0300

    itog

--- habits/views.py
+++ habits/views.py
@@ -5,23 +5,8 @@ from rest_framework import generics
 from habits.models import Habit
 from habits.paginators import HabitPagination
 from habits.serializers import HabitSerializer, PublicHabitSerializer
-from habits.services import create_replacements, create_schedule, create_task, make_replacements
+from habits.services import create_replacements, make_replacements
 from users.permissions import IsUser
-
-
-class HabitCreateAPIView(generics.CreateAPIView):
-    serializer_class = HabitSerializer
-
-    def perform_create(self, serializer):
-        habit = serializer.save(user=self.request.user)
-        if not habit.is_pleasant:
-            replacements = create_replacements(habit)
-            habit.frequency = make_replacements(habit.frequency, replacements)
-            habit.save()
-
-            if habit.user.tg_chat_id:
-                schedule = create_schedule(habit.frequency)
-                create_task(schedule, habit)
 
 
 class PublicHabitListAPIView(generics.ListAPIView):
