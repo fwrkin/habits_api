c6c2dc0e6ca750444307992aaeb758c9cfdf9a1a
fwrkin <fedotovvva23@mail.ru>
2025-06-03 15:50:11 +0300

    itog

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("habits.urls", namespace="habits")),
     path("users/", include("users.urls", namespace="users")),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/docs/", SpectacularRedocView.as_view(url_name="schema"), name="docs"),
-]
+    ]
