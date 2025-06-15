8e15aab902445a4456102df04aa7067ceb9b5dd6
fwrkin <fedotovvva23@mail.ru>
2025-06-15 12:32:17 +0300

    last work

--- users/migrations/0002_alter_user_managers.py
+++ users/migrations/0002_alter_user_managers.py
@@ -5,7 +5,9 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
 
-    dependencies = []
+    dependencies = [
+        ("users", "0001_initial"),
+    ]
 
     operations = [
         migrations.AlterModelManagers(
